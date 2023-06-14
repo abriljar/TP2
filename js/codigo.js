1ba25c3370cb1716fbc6b1c19a08de3c7996436a
abriljar <136550743+abriljar@users.noreply.github.com>
2023-06-14 10:05:56 -0300

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,28 @@
+document.getElementById('menu').addEventListener('click', function () {
+
+    document.getElementById('navega').classList.toggle('mostrar');
+
+
+})
+
+
+
+lightbox.option({
+    'wrapAround': true,
+    'albumLabel': "Imagen %1 de %2",
+    'fadeDuration': 2000,
+    'maxWidth': 1000,
+    'maxHeight': 1000
+    
+})
+
+
+
+document.getElementById('boton').addEventListener('click', function () {
+  if(document.getElementById('miVideo').paused)
+   document.getElementById('miVideo').play();
+else
+   document.getElementById('miVideo').pause();
+
+})
+
